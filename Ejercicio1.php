914439efce8a7a08a113e9e8e54a729cdbe0e960
JohnHeSaFe <henardsalango09@gmail.com>
2025-02-06 11:07:01 +0100

    Added messages and errors to each submit action of the form. Improved php structure. Fixed bugs.

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -5,8 +5,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<?php
+    session_start();
+
+    if (!isset($_SESSION['name'])) {
+        $_SESSION['name'] = "[No name]";
+    }
+    if (!isset($_SESSION['product'])) {
+        $_SESSION['product'] = [];
+    }
+
+    $message = "";
+    $error = "";
+    
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        if (isset($_POST['reset'])) {
+            session_unset();
+            session_destroy();
+            session_start();
+            $_SESSION['name'] = "[No name]";
+            $_SESSION['product'] = [];
+        }
+
+        if (!empty($_POST['username'])) {
+            $_SESSION['name'] = htmlspecialchars($_POST['username']);
+            $message = "Updated usename to ". $_SESSION['name'] . "<br><br>";
+        } 
+        
+        if (!empty($_POST['product']) && !empty($_POST['quantity'])) {
+            $product = htmlspecialchars($_POST['product']);
+            $quantity = (int) $_POST['quantity'];
+
+            if (isset($_POST['add'])) {
+                if (isset($_SESSION['product'][$product])) {
+                    $_SESSION['product'][$product] += $quantity;
+                    $message .= "Added $quantity units to $product";
+                } else {
+                    $_SESSION['product'][$product] = $quantity;
+                    $message .= "Added new product $product with $quantity units";
+                }
+            }
+
+            if (isset($_POST['remove'])) {
+                if (isset($_SESSION['product'][$product])) {
+                    $_SESSION['product'][$product] -= $quantity;
+                    $message = "Removed $quantity units to $product";
+
+                    if ($_SESSION['product'][$product] <= 0) {
+                        if ($_SESSION['product'][$product] < 0) {
+                            $error = "Quantity trying to remove to " . $product . " is greater than existent. Removing product instead";
+                        } else {
+                            $message = "Removed product $product";
+                        }
+                        unset($_SESSION['product'][$product]);
+                    }
+                } else {
+                    $error = $product . " is not on inventory, can't remove quantity";
+                }
+            }
+        }
+    }
+?>
+
 <body>
-    <form method="post">
+    <form method="post" action="">
         <h1>Supermarket management</h1>
         <label for="">Worker name:</label>
         <input type="text" name="username">
@@ -30,60 +93,22 @@
         <input type="submit" value="reset inventory" name="reset"> 
         <br>
 
+        <p style="color: green"><?php echo $message ?></p>
+        <p style="color: red"><?php echo $error ?></p>
+
         <h2>Inventory:</h2>
+        <p>worker: <?php echo $_SESSION["name"] ?></p>
+
+        <?php 
+        if (!empty($_SESSION['product'])) {
+            foreach ($_SESSION['product'] as $product => $quantity) {
+                echo "units " . $product . ": " . $quantity . "<br><br>";
+            }
+        } 
+        ?>
     </form>
 </body>
 
 <?php
-    if (!isset($_SESSION)) {
-        session_start();
-    }
-    
-    if (!isset($_SESSION['name'])) {
-        $_SESSION['name'] = "[No name]";
-    }
-    
-    if (!isset($_SESSION['product'])) {
-        $_SESSION['product'] = [];
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        if (isset($_POST['reset'])) {
-            session_unset();
-            session_destroy();
-            exit;
-        }
-
-        if (!empty($_POST['username'])) {
-            $_SESSION['name'] = htmlspecialchars($_POST['username']);
-        } 
-        
-        if (!empty($_POST['product']) && !empty($_POST['quantity'])) {
-            $product = htmlspecialchars($_POST['product']);
-            $quantity = (int) $_POST['quantity'];
-
-            if (isset($_POST['add'])) {
-                if (isset($_SESSION['product'][$product])) {
-                    $_SESSION['product'][$product] += $quantity;
-                } else {
-                    $_SESSION['product'][$product] = $quantity;
-                }
-            }
-
-            if (isset($_POST['remove']) && isset($_SESSION['product'][$product])) {
-                $_SESSION['product'][$product] -= $quantity;
-
-                if ($_SESSION['product'][$product] <= 0) {
-                    unset($_SESSION['product'][$product]);
-                }
-            } 
-        }
-    }
-
-    if (!empty($_SESSION['product'])) {
-        echo "worker: " . $_SESSION['name'] . "<br><br>";
-        foreach ($_SESSION['product'] as $product => $quantity) {
-            echo "units " . $product . ": " . $quantity . "<br><br>";
-        }
-    }  
+ 
 ?>
